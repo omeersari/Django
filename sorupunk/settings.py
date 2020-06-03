bee5318d572f7ee3ac864352bbd8cd2f67397c5c
omeersari <mryellow95@gmail.com>
2020-06-03 15:35:09 +0300

    edits

--- sorupunk/settings.py
+++ sorupunk/settings.py
@@ -161,5 +161,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mryellow122@gmail.com'
-EMAIL_HOST_PASSWORD = 'stilllovingyou'
+EMAIL_HOST_USER = 'emailiniz'
+EMAIL_HOST_PASSWORD = 'şifreniz'
